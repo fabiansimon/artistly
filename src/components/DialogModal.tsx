5e13359ea16ae9a8734f09c47941fae802c42f09
Fabian Simon <fabian.simon98@gmail.com>
2024-07-01 12:39:11 +0200

    timestamp comments added

--- src/components/DialogModal.tsx
+++ src/components/DialogModal.tsx
@@ -1,17 +1,25 @@
 import { motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
 
-function Modal({
+function DialogModal({
   isVisible,
   onRequestClose,
   children,
+  className,
+  contentClassName,
 }: {
   isVisible: boolean;
   onRequestClose?: () => void;
   children: React.ReactNode;
+  className?: string;
+  contentClassName?: string;
 }): JSX.Element {
   return (
     <motion.div
+      onClick={(e) => {
+        e.stopPropagation();
+        if (onRequestClose) onRequestClose();
+      }}
       initial="hidden"
       transition={{ duration: 0.08 }}
       animate={isVisible ? 'visible' : 'hidden'}
@@ -20,16 +28,18 @@ function Modal({
         hidden: { opacity: 0 },
       }}
       className={cn(
-        'fixed w-full h-full bg-black/50 content-center justify-center items-center',
-        !isVisible && 'pointer-events-none'
+        'fixed w-full h-full z-20 top-0 left-0 right-0 bottom-0 bg-black/50 content-center justify-center items-center',
+        !isVisible && 'pointer-events-none',
+        className
       )}
     >
       <motion.div
+        onClick={(e) => e.stopPropagation()}
         initial="hidden"
         transition={{ duration: 0.08 }}
         animate={isVisible ? 'visible' : 'hidden'}
         variants={{ visible: { scale: 1 }, hidden: { scale: 0.7 } }}
-        className="modal-box mx-auto"
+        className={cn('modal-box mx-auto', contentClassName)}
       >
         {children}
       </motion.div>
@@ -37,4 +47,4 @@ function Modal({
   );
 }
 
-export default Modal;
+export default DialogModal;
--- src/components/FeedbackInputModal.tsx
+++ src/components/FeedbackInputModal.tsx
@@ -1,15 +1,144 @@
-import Modal from './Modal';
+import { useEffect, useState } from 'react';
+import DialogModal from './DialogModal';
+import {
+  cn,
+  formatSeconds,
+  formattedTimeToNumber,
+  formatTimeInput,
+} from '@/lib/utils';
+import { REGEX } from '@/constants/regex';
+import { useAudioContext } from '@/providers/AudioProvider';
+
+interface FeedbackInputModalProps {
+  isVisible: boolean;
+  onRequestClose?: () => void;
+  timestamp?: number;
+  className?: string;
+}
 
 export default function FeedbackInputModal({
   isVisible,
   onRequestClose,
-}: {
-  isVisible: boolean;
-  onRequestClose?: () => void;
-}) {
+  timestamp,
+  className,
+}: FeedbackInputModalProps) {
+  const { file, addFeedback } = useAudioContext();
+
+  const [error, setError] = useState<string>('');
+  const [input, setInput] = useState<{ text: string; timestamp?: string }>({
+    text: '',
+  });
+
+  useEffect(() => {
+    if (!isVisible) {
+      setInput({ text: '' });
+      setError('');
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    setInput((prev) => ({
+      ...prev,
+      timestamp: timestamp ? formatSeconds(timestamp).toString() : undefined,
+    }));
+  }, [timestamp]);
+
+  const handleInput = (type: 'timestamp' | 'comment', value: string) => {
+    setInput((prev) => {
+      if (type === 'timestamp') {
+        const rawTime = formatTimeInput(value);
+        const error = checkError(rawTime);
+
+        setError(error);
+        return { ...prev, timestamp: rawTime };
+      }
+
+      return { ...prev, text: value };
+    });
+  };
+
+  const checkError = (value: string) => {
+    if (value.length === 0) return '';
+    if (!REGEX.timestamp.test(value))
+      return 'Timestamp in input must be format mm:ss';
+
+    const [minutes, seconds] = value.split(':').map(Number);
+    const timestamp = minutes * 60 + seconds;
+    if (timestamp > file?.duration!)
+      return 'Timestamp is outside of song duration.';
+
+    return '';
+  };
+
+  const postComment = () => {
+    const { text, timestamp } = input;
+    addFeedback({
+      text,
+      timestamp: timestamp ? formattedTimeToNumber(timestamp) : undefined,
+    });
+    if (onRequestClose) onRequestClose();
+  };
+
   return (
-    <Modal isVisible={isVisible}>
-      <div></div>
-    </Modal>
+    <DialogModal
+      onRequestClose={onRequestClose}
+      className={cn(className)}
+      isVisible={isVisible}
+    >
+      <div className="space-y-4">
+        <article className="prose">
+          <h3 className="font-medium text-white text-sm">{'Add comment'}</h3>
+        </article>
+        <div className="flex w-full space-x-2">
+          <input
+            value={input.text}
+            onInput={({ currentTarget: { value } }) =>
+              handleInput('comment', value)
+            }
+            className="textarea textarea-bordered bg-transparent w-full max-h-44"
+            placeholder="e.g I love the bass!"
+          />
+          <input
+            value={input.timestamp || undefined}
+            onInput={({ currentTarget: { value } }) =>
+              handleInput('timestamp', value)
+            }
+            className={cn(
+              'textarea textarea-bordered text-center bg-transparent w-20 max-h-44'
+            )}
+            placeholder="00:12"
+            pattern="^[0-5][0-9]\:[0-5][0-9]\.[0-9]{1,3}$"
+          />
+        </div>
+        <div className="modal-action">
+          <form method="dialog">
+            <button
+              className="btn btn-outline text-white/60"
+              onClick={onRequestClose}
+            >
+              {'Close'}
+            </button>
+            <button
+              onClick={postComment}
+              className="btn btn-primary ml-2"
+            >
+              {'Post'}
+            </button>
+          </form>
+        </div>
+        {error && (
+          <article className="prose mt-2 -mb-2">
+            <p
+              className={cn(
+                'text-red-500/90 text-sm opacity-0',
+                error && 'opacity-1'
+              )}
+            >
+              {error || '1'}
+            </p>
+          </article>
+        )}
+      </div>
+    </DialogModal>
   );
 }
