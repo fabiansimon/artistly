bf86ff4c424544ddc731f8fa271a112837f9505d
Fabian Simon <fabian.simon98@gmail.com>
2024-07-01 14:22:16 +0200

    dual range slider init

--- src/components/RangeIndicator.tsx
+++ src/components/RangeIndicator.tsx
@@ -1,44 +1,73 @@
 import { cn } from '@/lib/utils';
 import { useAudioContext } from '@/providers/AudioProvider';
-import { Range } from '@/types';
-import { motion } from 'framer-motion';
 import { useEffect, useMemo, useState } from 'react';
 
 export default function RangeIndicator({ className }: { className?: string }) {
-  const { audioRef, range, setRange } = useAudioContext();
-  const [hovered, setHovered] = useState<boolean>(false);
+  const {
+    range: { begin, end },
+    setRange,
+  } = useAudioContext();
+
+  const [minPrice, setMinPrice] = useState(1000);
+  const [maxPrice, setMaxPrice] = useState(7000);
+  const [minThumb, setMinThumb] = useState(0);
+  const [maxThumb, setMaxThumb] = useState(0);
+
+  const min = 100;
+  const max = 10000;
 
   useEffect(() => {
-    if (!audioRef.current) return;
-    setRange({ begin: 0, end: 40 });
-  }, [audioRef, setRange]);
+    minTrigger();
+    maxTrigger();
+  }, [minPrice, maxPrice]);
 
-  const { left, right } = useMemo(() => {
-    console.log(audioRef.current?.currentSrc);
-    if (!audioRef.current) return { left: 0, right: 0 };
-    const { begin, end } = range;
+  const minTrigger = () => {
+    setMinPrice((prevMin) => Math.min(prevMin, maxPrice - 500));
+    setMinThumb(((minPrice - min) / (max - min)) * 100);
+  };
 
-    const duration = audioRef.current.duration;
-    const l = (begin / duration) * 100;
-    const r = (end / duration) * 100;
-    return { left: `${l}%`, right: `${r}%` };
-  }, [range, audioRef]);
+  const maxTrigger = () => {
+    setMaxPrice((prevMax) => Math.max(prevMax, minPrice + 500));
+    setMaxThumb(100 - ((maxPrice - min) / (max - min)) * 100);
+  };
 
   return (
-    <div
-      className={cn(
-        'flex flex-grow w-full h-1 overflow-visible bg-yellow-500/30 relative',
-        className
-      )}
-    >
-      <div
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className={
-          'flex cursor-pointer absolute h-full bg-yellow-500 left-[10px]'
-        }
-        style={{ left, right }}
-      />
+    <div className={cn('relative', className)}>
+      <div>
+        <input
+          type="range"
+          step="100"
+          min={min}
+          max={max}
+          value={minPrice}
+          onChange={(e) => setMinPrice(Number(e.target.value))}
+          className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+        />
+        <input
+          type="range"
+          step="100"
+          min={min}
+          max={max}
+          value={maxPrice}
+          onChange={(e) => setMaxPrice(Number(e.target.value))}
+          className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+        />
+        <div className="relative z-10 h-1">
+          <div className="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-yellow-400/30"></div>
+          <div
+            className="absolute z-20 top-0 bottom-0 bg-yellow-500"
+            style={{ right: `${maxThumb}%`, left: `${minThumb}%` }}
+          />
+          <div
+            className="absolute z-30 w-1 h-4 top-0 bg-yellow-500 rounded-xs -mt-[5px] -ml-1"
+            style={{ left: `${minThumb}%` }}
+          />
+          <div
+            className="absolute z-30 w-1 h-4 top-0 bg-yellow-500 rounded-xs -mt-[5px]"
+            style={{ right: `${maxThumb}%` }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
