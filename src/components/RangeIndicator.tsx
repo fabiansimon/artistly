08ef6c27888dc8c422e0adfb0b47fe05d14c0ebb
Fabian Simon <fabian.simon98@gmail.com>
2024-07-02 10:47:06 +0200

    fixed range indicator

--- src/components/RangeIndicator.tsx
+++ src/components/RangeIndicator.tsx
@@ -3,47 +3,53 @@ import { useAudioContext } from '@/providers/AudioProvider';
 import { useEffect, useMemo, useState } from 'react';
 
 export default function RangeIndicator({ className }: { className?: string }) {
-  const { file, range, setRange } = useAudioContext();
+  const {
+    file,
+    settings: { looping },
+    range: { begin, end },
+    setRange,
+  } = useAudioContext();
 
   const min = 0;
   const max = Math.round(file?.duration!);
 
-  const [left, setLeft] = useState(min);
-  const [right, setRight] = useState(max);
+  useEffect(() => {
+    setRange({ begin: min, end: max });
+  }, []);
 
   const thumbLeft = useMemo(() => {
-    return ((left - min) / (max - min)) * 100;
-  }, [left, max]);
+    return ((begin - min) / (max - min)) * 100;
+  }, [begin, max]);
 
   const thumbRight = useMemo(() => {
-    return 100 - ((right - min) / (max - min)) * 100;
-  }, [right, max]);
+    return 100 - ((end - min) / (max - min)) * 100;
+  }, [end, max]);
 
   useEffect(() => {
     maxTrigger();
     minTrigger();
-    setRange({ begin: left, end: right });
-    // console.log('Minimum', left, 'Maximum', right);
-  }, [left, right]);
+  }, [begin, end]);
 
   const minTrigger = () => {
-    setLeft((prev) => Math.min(prev, right - 1));
+    setRange((prev) => ({ ...prev, begin: Math.min(prev.begin, end - 1) }));
   };
 
   const maxTrigger = () => {
-    setRight((prev) => Math.max(prev, left + 1));
+    setRange((prev) => ({ ...prev, end: Math.max(prev.end, begin + 1) }));
   };
 
   return (
-    <div className={cn('relative', className)}>
+    <div className={cn('relative', !looping && 'opacity-10', className)}>
       <div>
         <input
           type="range"
           step="1"
           min={min}
           max={max}
-          value={left}
-          onChange={(e) => setLeft(Number(e.target.value))}
+          value={begin}
+          onChange={(e) =>
+            setRange((prev) => ({ ...prev, begin: Number(e.target.value) }))
+          }
           className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
         />
         <input
@@ -51,14 +57,17 @@ export default function RangeIndicator({ className }: { className?: string }) {
           step="1"
           min={min}
           max={max}
-          value={right}
-          onChange={(e) => setRight(Number(e.target.value))}
+          value={end}
+          onChange={(e) =>
+            setRange((prev) => ({ ...prev, end: Number(e.target.value) }))
+          }
           className="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
         />
         <div className="relative z-10 h-1">
-          <div className="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-yellow-400/30"></div>
+          <div className="absolute z-10 begin-0 right-0 bottom-0 top-0 h-full w-full bg-yellow-500/30 rounded-md"></div>
           <div
-            className="absolute z-20 top-0 bottom-0 bg-yellow-500"
+            onDoubleClick={() => setRange({ begin: min, end: max })}
+            className="absolute z-20 cursor-pointer top-0 bottom-0 bg-yellow-500"
             style={{ right: `${thumbRight}%`, left: `${thumbLeft}%` }}
           />
           <div
